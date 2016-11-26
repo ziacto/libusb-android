df496bbfc538f2143e4822dfb69cbbeaf5009c1b
Julio Tentor <jtentor@gmail.com>
2016-11-26 10:18:46 -0300

    Show different message

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ jstring
 Java_org_libusb_1android_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++\n\nThis is native-lib.cpp in main/cpp from libUSB-Android demo application";
     return env->NewStringUTF(hello.c_str());
 }
